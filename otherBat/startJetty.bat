f18b4af057ef407772ec1aaf726861c37326b80e
欧初权(sfit1014)-顺丰科技 <sfit1014@sf.com>
2016-02-26 18:32:05 +0800

    specufy port on command line when jetty strart

--- otherBat/startJetty.bat
+++ otherBat/startJetty.bat
@@ -4,6 +4,6 @@ set CLASSPATH=%JAVA_HOME%\lib\dt.jar;%JAVA_HOME%\lib\tools.jar
 set GRADLE_HOME=%cd%\gradle
 set PATH=%JAVA_HOME%\bin;%GRADLE_HOME%\bin;%PATH%
 
-set JETTY_HOME=D:\user\sfit1014\Downloads\software\jetty9
-java -jar D:\user\sfit1014\Downloads\software\jetty9\start.jar
+set JETTY_HOME=D:\user\xx\Downloads\software\jetty9
+java -jar D:\user\xx\Downloads\software\jetty9\start.jar jetty.http.port=8081
 
